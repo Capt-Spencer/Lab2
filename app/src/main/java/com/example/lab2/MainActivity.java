dd9ccd2aa10082403ec6c44600c786bfb08794aa
Keegan <keegan@kairuni.com>
2018-04-05 14:40:59 -0700

    Added a fancy toast.

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lab2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast toast = Toast.makeText(this, "A Keegan Toast!", Toast.LENGTH_LONG);
+        toast.show();
     }
 }
